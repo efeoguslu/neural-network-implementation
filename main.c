c7caf824d0a8bafb090dd2dabda680b20a097b36
efeoguslu <efeoguslu@gmail.com>
2023-12-26 15:58:34 +0300

    add stb_image.h library

--- main.c
+++ main.c
@@ -4,8 +4,13 @@
 #include "matrix.h"
 #include "mnist.h"
 #include <time.h>
+#include <dirent.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // training data:
+
+// 41.03
 
 double td[] = {
     0, 0, 0,
@@ -14,7 +19,63 @@ double td[] = {
     1, 1, 0,
 };
 
-int main(){
+char *args_shift(int *argc, char ***argv){
+    assert(*argc > 0);
+    char *result = **argv;
+    (*argc) -= 1;
+    (*argv) += 1;
+    return result;
+}
+
+int main(int argc, char **argv){
+
+    const char *program = args_shift(&argc, &argv);
+
+    if(argc <= 0){
+        fprintf(stderr, "Usage: %s <input>\n", program);
+        fprintf(stderr, "ERROR: no input file provided\n");
+        return 1;
+    }
+
+    const char *img_file_path = args_shift(&argc, &argv);
+
+    int img_width, img_height, img_comp;
+    uint8_t *img_pixels = (uint8_t *)stbi_load(img_file_path, &img_width, &img_height, &img_comp, 0);
+
+    if(img_pixels == NULL){
+        fprintf(stderr, "ERROR: could not read image %s\n", img_file_path);
+        return 1;
+    }
+
+    if(img_comp != 1){
+        fprintf(stderr, "ERROR: %s is %d bits image. Only 8 bit grayscale images are supported.\n", img_file_path, img_comp*8);
+        return 1;
+    }
+
+    printf("%s size %dx%d %d bits\n", img_file_path, img_width, img_height, img_comp*8);
+
+    Mat t = mat_alloc(img_width*img_height, 3);
+
+    for(int y = 0; y < img_height; ++y){
+        for(int x = 0; x < img_width; ++x){
+            size_t i = y*img_width + x;
+            double nx = (double)x/(img_width - 1);
+            double ny = (double)y/(img_height - 1);
+            double nb = img_pixels[i]/255.0;
+            MAT_AT(t, i, 0) = nx;
+            MAT_AT(t, i, 1) = ny;
+            MAT_AT(t, i, 2) = nb;
+        }
+    }
+
+    MAT_PRINT(t);
+
+    const char *out_file_path = "img.mat";
+    FILE *out = fopen(out_file_path, "wb");
+    // void mat_save(FILE *out, Mat m);
+
+    // ---------------------------------------------------------------------------------------------
+    return 0;
 
     randomize();
     
